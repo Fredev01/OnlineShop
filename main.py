5b9ecea5865ed89cc5b916320c190bb33dd18214
Jesus <jesusguzmanjimenez53@gmail.com>
2024-11-03 18:48:00 -0600

    integracion de CSRF

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from flask import Flask, redirect, render_template, url_for, session
+from flask_wtf import CSRFProtect
 import pymysql
-from features import settings
-from features import db
+from features import settings, db  
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = settings.SECRET_KEY
+# Configuración de la aplicación
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default_secret_key")  # Carga la clave desde .env o usa un valor por defecto
 app.config["SQLALCHEMY_DATABASE_URI"] = settings.DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Inicializa la base de datos y la protección CSRF
 db.init_app(app)
-
+csrf = CSRFProtect(app)  # Habilita CSRF en la aplicación
 
 @app.route('/')
 def home():
@@ -22,6 +28,7 @@ def home():
     Returns:
         html template: Returns the Dashboard or Index
     """
+    return render_template('index.html')
 
 @app.route('/dashboard')
 def dashboard():
